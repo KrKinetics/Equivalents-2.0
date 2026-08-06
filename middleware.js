fa010047f1f7bb079f1bb93dcb4738adb22c6063
KrKinetics <kev.racine@krkinetics.com>
2026-08-06 13:46:50 -0400

    fix(reviews): secure moderation with actual master id

--- middleware.js
+++ middleware.js
@@ -14,7 +14,7 @@ import {
   buildWorkspaceCspPolicy,
 } from './src/coach/server/http/csp-policy.mjs';
 
-const MASTER_USER_ID = '14376851-b293-49a3-b0cb-d97117b2a27f';
+const MASTER_USER_ID = '143f2b15-5d24-4992-b648-42c43bd1e802';
 
 export const config = {
   matcher: [
